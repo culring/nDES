79f33ebfde6410cfe65e49c8db29d70700700bab
Rafał Wójcik <rafal.wojcik@nexio.pl>
2021-09-13 20:10:54 +0200

    Bugs in IterativeLoadingStrategy fixed

--- src/ndes/distribution_strategy/iterative_loading_strategy.py
+++ src/ndes/distribution_strategy/iterative_loading_strategy.py
@@ -14,52 +14,57 @@ class IterativeLoadingStrategy(DistributionStrategy):
         population_size = population.shape[1]
         current_individual_idx = 0
         total_batch_indices = []
+        individuals_left = population_size
 
-        while current_individual_idx < population_size:
+        while individuals_left > self.max_individuals_per_node * self.num_nodes:
+            for node in self.nodes:
+                end_individual_idx = current_individual_idx + self.max_individuals_per_node
+                individuals = population[:, current_individual_idx:end_individual_idx]
+                current_individual_idx = end_individual_idx
+                node.load_individuals(individuals)
+
+                batches = self._get_next_batches(self.max_individuals_per_node)
+                node.load_batches(batches)
+
+                batch_indices = [idx for idx, _ in batches]
+                total_batch_indices.extend(batch_indices)
+
+                batch_order = range(self.max_individuals_per_node)
+                node.evaluate(batch_order)
+
+            for node in self.nodes:
+                result = node.get_fitness()
+                fitness_values.extend(result)
             individuals_left = population_size - current_individual_idx
-            if individuals_left > self.max_individuals_per_node * self.num_nodes:
-                for node in self.nodes:
-                    end_individual_idx = current_individual_idx + self.max_individuals_per_node
-                    individuals = population[current_individual_idx:end_individual_idx]
-                    current_individual_idx = end_individual_idx
-                    node.load_individuals(individuals)
 
-                    batches = self._get_next_batches(self.max_individuals_per_node)
-                    node.load_batches(batches)
+        if individuals_left > 0:
+            num_individuals_per_node = individuals_left // self.num_nodes
+            remaining_individuals_num = individuals_left - num_individuals_per_node * self.num_nodes
+            for node in self.nodes:
+                num_individuals = num_individuals_per_node
+                if remaining_individuals_num > 0:
+                    remaining_individuals_num -= 1
+                    num_individuals += 1
 
-                    batch_indices = [idx for idx, _ in batches]
-                    total_batch_indices.extend(batch_indices)
+                end_individual_idx = current_individual_idx + num_individuals
+                individuals = population[:, current_individual_idx:end_individual_idx]
+                current_individual_idx = end_individual_idx
+                node.load_individuals(individuals)
 
-                    batch_order = range(self.max_individuals_per_node)
-                    node.evaluate(batch_order)
-            else:
-                num_individuals_per_node = population_size // self.num_nodes
-                remaining_individuals = population_size - num_individuals_per_node * self.num_nodes
-                for node in self.nodes:
-                    num_individuals = num_individuals_per_node
-                    if remaining_individuals > 0:
-                        remaining_individuals -= 1
-                        num_individuals += 1
+                batches = self._get_next_batches(num_individuals)
+                node.load_batches(batches)
 
-                    end_individual_idx = current_individual_idx + num_individuals
-                    individuals = population[:, current_individual_idx:end_individual_idx]
-                    current_individual_idx = end_individual_idx
-                    node.load_individuals(individuals)
+                batch_indices = [idx for idx, _ in batches]
+                total_batch_indices.extend(batch_indices)
 
-                    batches = self._get_next_batches(num_individuals)
-                    node.load_batches(batches)
-
-                    batch_indices = [idx for idx, _ in batches]
-                    total_batch_indices.extend(batch_indices)
-
-                    batch_order = range(num_individuals)
-                    node.evaluate(batch_order)
+                batch_order = range(num_individuals)
+                node.evaluate(batch_order)
 
             for node in self.nodes:
                 result = node.get_fitness()
                 fitness_values.extend(result)
 
-            return fitness_values, total_batch_indices
+        return fitness_values, total_batch_indices
 
     def _get_next_batches(self, num_batches):
         return [next(self.data_gen) for _ in range(num_batches)]
